9001548689b15701cc493b3324febbf2dfe39b71
Ronaldo Queiroz <ronquesil@gmail.com>
2023-05-12 21:51:52 -0300

    Create Variaveis_Keywords.py

---
+++ Variaveis_Keywords.py
@@ -0,0 +1,13 @@
+# A criação de uma variável é realizada por meio da declaração e a atribuição de um valor a ela.
+#
+#variavel = valor
+
+pessoa = 'Ronaldo Queiroz da Silva'
+idade = 36
+altura = 1.86
+print(pessoa, idade, altura)
+
+texto = 'Este é um texto muito extenso'
+porcentagem = 0.5
+numero = -10
+print(texto)
